2d99dc1b5589b7f8273cdd4377e377870f2e80cb
Patryk Frankowski <patryk.frankowski@capgemini.com>
2021-09-06 18:32:49 +0200

    Add Abstract and DAOs Tests.

--- src/test/java/org/tkit/app/domain/daos/BookDAOTest.java
+++ src/test/java/org/tkit/app/domain/daos/BookDAOTest.java
@@ -1,55 +1,55 @@
-//package org.tkit.app.domain.daos;
-//
-//import io.quarkus.test.junit.QuarkusTest;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.tkit.app.domain.models.criteria.BookSearchCriteria;
-//import org.tkit.quarkus.jpa.exceptions.DAOException;
-//
-//import static org.assertj.core.api.Assertions.*;
-//
-//import javax.persistence.EntityManager;
-//
-//@QuarkusTest
-//class BookDAOTest {
-//
-//    @Test
-//    void shouldThrowDAOExceptionWhenTryFindBookByCriteriaEqualNull() {
-//
-//        /* Given */
-//        BookDAO bookDAO = new BookDAO() {
-//            @Override
-//            protected EntityManager getEntityManager() {
-//                return null;
-//            }
-//        };
-//
-//        /* When */
-//        DAOException thrownException = Assertions.assertThrows(DAOException.class, () ->
-//                bookDAO.searchByCriteria(null));
-//
-//        /* Then */
-//        assertThat(thrownException.key).isEqualTo(BookDAO.ErrorKeys.ERROR_FIND_BOOKS_SEARCH_CRITERIA_REQUIRED);
-//    }
-//
-//    @Test
-//    void shouldThrowDAOExceptionWhenTryFindTableByEmptyCriteria() {
-//
-//        /* Given */
-//        BookDAO bookDAO = new BookDAO() {
-//            @Override
-//            protected EntityManager getEntityManager() {
-//                return null;
-//            }
-//        };
-//
-//        BookSearchCriteria bookSearchCriteria = new BookSearchCriteria();
-//
-//        /* When */
-//        DAOException thrownException = Assertions.assertThrows(DAOException.class, () ->
-//                bookDAO.searchByCriteria(bookSearchCriteria));
-//
-//        /* Then */
-//        assertThat(thrownException.key).isEqualTo(BookDAO.ErrorKeys.ERROR_FIND_BOOKS_BY_CRITERIA);
-//    }
-//}
+package org.tkit.app.domain.daos;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.tkit.app.domain.models.criteria.BookSearchCriteria;
+import org.tkit.quarkus.jpa.exceptions.DAOException;
+
+import static org.assertj.core.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+
+@QuarkusTest
+class BookDAOTest {
+
+    @Test
+    void shouldThrowDAOExceptionWhenTryFindBookByCriteriaEqualNull() {
+
+        /* Given */
+        BookDAO bookDAO = new BookDAO() {
+            @Override
+            protected EntityManager getEntityManager() {
+                return null;
+            }
+        };
+
+        /* When */
+        DAOException thrownException = Assertions.assertThrows(DAOException.class, () ->
+                bookDAO.searchByCriteria(null));
+
+        /* Then */
+        assertThat(thrownException.key).isEqualTo(BookDAO.ErrorKeys.ERROR_FIND_BOOKS_SEARCH_CRITERIA_REQUIRED);
+    }
+
+    @Test
+    void shouldThrowDAOExceptionWhenTryFindBookByEmptyCriteria() {
+
+        /* Given */
+        BookDAO bookDAO = new BookDAO() {
+            @Override
+            protected EntityManager getEntityManager() {
+                return null;
+            }
+        };
+
+        BookSearchCriteria bookSearchCriteria = new BookSearchCriteria();
+
+        /* When */
+        DAOException thrownException = Assertions.assertThrows(DAOException.class, () ->
+                bookDAO.searchByCriteria(bookSearchCriteria));
+
+        /* Then */
+        assertThat(thrownException.key).isEqualTo(BookDAO.ErrorKeys.ERROR_FIND_BOOKS_BY_CRITERIA);
+    }
+}
---
+++ src/test/java/org/tkit/app/rs/v1/controllers/BookRestControllerTest.java
@@ -0,0 +1,9 @@
+//package org.tkit.app.rs.v1.controllers;
+//
+//import io.quarkus.test.junit.QuarkusTest;
+//import org.tkit.quarkus.test.WithDBData;
+//
+//@QuarkusTest
+//@WithDBData()
+//public class BookRestControllerTest {
+//}
